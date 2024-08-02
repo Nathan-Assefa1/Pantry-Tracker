85558604e164401da904796058d9ff2b135c863b
Nathan Assefa <assefanathan@gmail.com>
2024-08-02 17:07:44 -0400

    ,

--- app/page.js
+++ app/page.js
@@ -63,99 +63,92 @@ export default function Home(){
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
       
-  return(
+  return (
     <Box
-    width = "100vw" //The full length of the screen
-    height = "100vh"
-    display='flex'
-    flexDirection={'column'}
-    justifyContent="center" //Centers it horizontally
-    alignItems='center' //Centers it vertically
-    gap={2}
+      width="100vw"
+      height="100vh"
+      display={'flex'}
+      justifyContent={'center'}
+      flexDirection={'column'}
+      alignItems={'center'}
+      gap={2}
     >
       <Modal
         open={open}
-        onClose={handleClose}>
-          <Box //The properties are to make the box truly centered
-          position='absolute'
-          top='50%' left='50%'
-          width={400} bgcolor='white'
-          border='2px solid #000'
-          boxShadow={24} p={4} display='flex' flexDirection='column' gap={3}
-          sx={{ //Using custom style instead of pre-built components
-            transform:'translate(-50%,-50%)'
-          }}>
-            <Typography variant='h6'>Add Item</Typography>
-            <Stack //Organizes everything inside in a stack format 
-            width='100%'
-            direction='row' //stacks in a horizontal way
-            spacing={2} //So verything isn't smushed
-            >  
-              <TextField //Saves the value of user input
-              variant='outlined' 
-              fullwidth 
-              value={itemName} 
-              onChange={(e)=>{
-                setItemName(e.target.value)
-              }}/>
-              <Button variant="outlined" onClick={() => {
-                addItems(itemName) //Calls helper function
-                setItemName('') //Refresh state variable
-                handleClose() //Close model
-              }}>Add</Button>
-            </Stack>
-          </Box>
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Add Item
+          </Typography>
+          <Stack width="100%" direction={'row'} spacing={2}>
+            <TextField
+              id="outlined-basic"
+              label="Item"
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
+            />
+            <Button
+              variant="outlined"
+              onClick={() => {
+                addItem(itemName)
+                setItemName('')
+                handleClose()
+              }}
+            >
+              Add
+            </Button>
+          </Stack>
+        </Box>
       </Modal>
-
-      <Button variant='contained' onClick={handleOpen}>
+      <Button variant="contained" onClick={handleOpen}>
         Add New Item
       </Button>
-
-      <Box border='1px solid #333'>
+      <Box border={'1px solid #333'}>
         <Box
-        width='800px'
-        height='100px'
-        bgcolor='#ADD8E6'
-        display = 'flex'
-        alignItems='center'
-        justifyContent='center'
+          width="800px"
+          height="100px"
+          bgcolor={'#ADD8E6'}
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={'center'}
         >
-          <Typography variant='h2' color='#333' textAlign="center">
+          <Typography variant={'h2'} color={'#333'} textAlign={'center'}>
             Inventory Items
           </Typography>
         </Box>
+        <Stack width="800px" height="300px" spacing={2} overflow={'auto'}>
+          {items.map(({name, quantity}) => (
+            <Box
+              key={name}
+              width="100%"
+              minHeight="150px"
+              display={'flex'}
+              justifyContent={'space-between'}
+              alignItems={'center'}
+              bgcolor={'#f0f0f0'}
+              paddingX={5}
+            >
+              <Typography variant={'h3'} color={'#333'} textAlign={'center'}>
+                {name.charAt(0).toUpperCase() + name.slice(1)}
+              </Typography>
+              <Typography variant={'h3'} color={'#333'} textAlign={'center'}>
+                Quantity: {quantity}
+              </Typography>
+              <Button variant="contained" onClick={() => removeItem(name)}>
+                Remove
+              </Button>
+            </Box>
+          ))}
+        </Stack>
       </Box>
-      <Stack width='800px' height='300px' spacing={2} overflow='auto'>
-       {
-          
-          items.map(({name, count}) => (
-          <Box
-          key={name}
-          width='100%'
-          minHeight='150px'
-          display='flex'
-          justifyContent='space-between'
-          alignitems='center'
-          bgcolor='#f0f0f0'
-          paddingX={5}
-          >
-            <Typography variant='h3' color='#333' >
-              {name.charAt(0).toUpperCase() + name.slice(1)}
-            </Typography>
-            <Typography variant='h3' color='#333' alignItems={'center'}>
-              {count}
-            </Typography>
-            <Stack direction='row' spacing={2}>
-              <Button variant='contained' onClick={() => addItems(name)}>Add</Button>
-              <Button variant='contained' onClick={() => removeItems(name)}>Remove</Button>
-            </Stack>
-          </Box>))
-
-          
-          
-        }
-
-      </Stack>
     </Box>
   )
+
+          
+      
 }
